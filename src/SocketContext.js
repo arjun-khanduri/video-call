897df8ca839d74ccad214631033f8704a82a84aa
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-11-04 11:43:25 +0530

    ft: socket client useEffect code

---
+++ src/SocketContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useState, useRef, useEffect } from "react";
+import { io } from "socket.io-client";
+import Peer from "simple-peer";
+
+const SocketContext = createContext();
+const socket = io("http://localhost:8080");
+const ContextProvider = ({ children }) => {
+  const [stream, setStream] = useState(null);
+  const [me, setMe] = useState("");
+  const [call, setCall] = useState({});
+  const video = useRef();
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        setStream(stream);
+        video.current.srcObject = stream;
+      });
+    socket.on("me", (id) => setMe(id));
+    socket.on("calluser", ({ from, name: callerName, signal }) => {
+      setCall({ isReceivedCall: true, from, name: callerName, signal });
+    });
+  }, []);
+  const receiveCall = () => {};
+  const initCall = () => {};
+  const disconnectCall = () => {};
+};
