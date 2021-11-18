15bf0cd4cbfbafb2556c69529843b6cbe0f824ad
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-11-18 23:40:59 +0530

    ft: call alert panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import VideoFeed from './components/VideoFeed';
 import Panel from './components/Panel';
+import Alert from './components/Alert';
 
 const App = () => {
   return (
     <>
       <VideoFeed />
-      <Panel />
+      <Panel>
+        <Alert />
+      </Panel>
     </>
   )
 };
