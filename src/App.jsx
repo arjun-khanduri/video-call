39c399e8c2b3de7673629170b92de1f0fcf9e30d
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-12-22 13:25:35 +0530

    style: antd css added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import VideoFeed from './components/VideoFeed';
 import Panel from './components/Panel';
 import Alert from './components/Alert';
+import 'antd/dist/antd.css';
 
 const App = () => {
   return (
