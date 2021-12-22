3daf3bc1c5231eaaa38f1aea965755ae9d56e2a7
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-12-22 13:26:13 +0530

    style: alert panel styled

--- src/components/Alert/index.js
+++ src/components/Alert/index.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react'
 
 import { SocketContext } from '../../SocketContext';
+import { Card, Button, Row, Col } from 'antd';
+// import './index.css';
 
 const Alert = () => {
     const {
@@ -13,17 +15,23 @@ const Alert = () => {
 
     return (
         <>
-            {call.isReceivingCall && !callReceived && (
-                <div>
-                    <h1>Calling '{call.name}'...</h1>
-                    <button onClick={receiveCall}>
-                        Receive Call
-                    </button>
-                    <button onClick={() => window.location.reload()}>
-                        Reject Call
-                    </button>
-                </div>
-            )}
+            <Row>
+                <Col span={10}></Col>
+                {call.isReceivingCall && !callReceived && (
+                    <Col span={12}>
+                        <Card class="alert" style={{ width: '50%', textAlign: 'center' }}>
+                            <h1>Calling '{call.name}'...</h1>
+                            <Button type="primary" onClick={receiveCall}>
+                                Receive Call
+                            </Button>
+                            <Button type="danger" onClick={() => window.location.reload()}>
+                                Reject Call
+                            </Button>
+                        </Card>
+                    </Col>
+                )}
+
+            </Row>
         </>
     )
 }
